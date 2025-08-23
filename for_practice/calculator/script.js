ab4bba2f602768116092bde7a7bdbaabaec55f8d
theboogeyman81 <theboogeyman8144@gmail.com>
2025-08-23 06:21:42 +0530

    code

--- for_practice/calculator/script.js
+++ for_practice/calculator/script.js
@@ -1,61 +1,21 @@
-let display = document.getElementById('display');
-let currentInput= '';
-let previousInput = '';
 
 function appendToDisplay(value){
-
+    document.getElementById('display').value += value;
 }
 
 
 function calculate(){
-    if(previousInput !== '' && currentInput !== '' && operator !== ''){
-        let result;
-        let prev = parseFloat(previousInput);
-        let current = parseFloat(currentInput);
-
-        switch(operator){
-            case '+':
-                result = prev + current;
-
-            case '-':
-                result = prev - current;
-                break;
-            case '*':
-                result = prev * current;
-                break;
-
-            case '/':
-                if(current === 0){
-                    display.value = 'Error';
-                    resetCalculator();
-                    return;
-                }
-                result = prev/current;
-        }
-        display.value = result;
-        currentInput = result.toString();
-        previousInput= '';
-        operator = '';
+    try{
+        let result  = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    }catch(error){
+        document.getElementById('display').value = 'Error';
     }
 }
 
 
 function clearDisplay(){
-    display.value = '';
-    currentInput = '';
-    previousInput = '';
-    operator = '';
+    document.getElementById('display').value = '';
 }
 
 
-function deleteLast(){
-    if(currentInput !== ''){
-        currentInput = currentInput.slice(0,-1);
-    }
-}
-
-function resetCalculator(){
-    setTimeout(()=>{
-        clearDisplay();
-    },1500);
-}
